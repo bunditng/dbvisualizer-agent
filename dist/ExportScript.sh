24d0570494417a578938256e8fe809ef745254c7
Bundit.Ng <siam.arm@gmail.com>
2020-11-03 10:53:16 +0700

    update recieve and process args

--- dist/ExportScript.sh
+++ dist/ExportScript.sh
@@ -1,5 +1,8 @@
 echo -e "\n [*] Generating dbvis.lecense "
-java -jar dbvisualizer-agent.jar -i ImposterSoft -n AbStBdNg -o ImposterSoft Ltd. | grep dbvis | dd of=~/.dbvis/dbvis.license
+read -p "Enter ID : " puid
+read -p "Enter Name : " pname
+read -p "Enter Organization : " porg
+java -jar dbvisualizer-agent.jar -i ${puid} -n ${pname} -o ${porg} | grep dbvis | dd of=~/.dbvis/dbvis.license
 echo -e " [*] Done !! "
 cp dbvisualizer-agent.jar ~/.dbvis/dbvisualizer-agent.jar
 echo -e -javaagent:~/.dbvis/dbvisualizer-agent.jar | dd of=~/.dbvis/user.vmoptions
